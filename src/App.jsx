3f721c8d88a78641c186368d58a33df4ef023861
Abhiraj412 <abhangparth@gmail.com>
2024-06-13 12:08:19 +0530

    basic play using react

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
+
+// var todos = [
+// {
+//   title:"GO to gym",
+//   description: "Hit the gym and grind",
+//   id: 1
+// },
+// {
+//   title: "Go eat lunch",
+//   description: "You are hungry go eat food",
+//   id: 2
+// }]
+
 
 function App() {
+  const [todo, setTodo] = useState({
+    title:"GO to gym",
+    description: "Hit the gym and grind",
+    id: 1
+})
+
+setInterval(() => {
+  setTodo({
+    title: "Workout",
+    description: "Cardio time",
+    id: 100
+  })
+}, 2000)
+
   return (
     <>
       <h1>Hi There!!</h1>
+      {todo.title}
+      {todo.description}
+      {todo.id}
     </>
   )
 }
